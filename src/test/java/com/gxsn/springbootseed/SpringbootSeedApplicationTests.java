6021208b606e806bb73bd112f8c3aa48ca56f4cf
yangzh <1214438717@qq.com>
2022-01-20 10:27:36 +0800

    初始化springboot工程

---
+++ src/test/java/com/gxsn/springbootseed/SpringbootSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxsn.springbootseed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
